fcc40d93681b10fd43776b02da7a310381f4aa6c
Vccentric <>
2018-07-21 17:20:26 -0700

    Added package stub files and folders.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # React-Quiz
 This is a simple quiz application built with React.js
+
+This is currently a work in progress.
---
+++ js/reactQuiz.js
@@ -0,0 +1,14 @@
+/**
+ * React Quiz v1.0.0
+ * This is simple quiz application built using React.js
+ *
+ * @author      Christopher Viray
+ * @copyright   (c) 2018 Christopher Viray
+ * @license     MIT
+ * @version     1.0.0
+ */
+
+ReactDOM.render(
+    <h1>Hello World!!!</h1>,
+    document.getElementById('root')
+);
