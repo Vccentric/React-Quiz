4b5ec8743638cff5f8e61b151ad68cf635929495
Vccentric <35283237+Vccentric@users.noreply.github.com>
2018-11-05 18:18:42 -0800

    Add initial files for the new project structure.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { hot } from 'react-hot-loader';
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <h1> Hello, World!!!</h1>
+        </div>
+    );
+}
+
+export default hot(module)(App);
