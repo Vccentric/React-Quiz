6021b215688f573ebd266b9dd0b9a8f2914c5b75
Vccentric <35283237+Vccentric@users.noreply.github.com>
2018-11-09 21:56:17 -0800

    1. Update package dependencies. 2. Update formatting in CSS file. 3. Add 'Popup' Compnent for 'redux' build.

---
+++ src/redux/components/Popup.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { hot } from "react-hot-loader";
+
+// popup component
+const Popup = ({
+ title, text, handleButtonClick, buttonText,
+}) => (
+    <div className="popup">
+        <h1>{title}</h1>
+        <p>{text}</p>
+        <button type="button" className="button" onClick={handleButtonClick}>
+            {buttonText}
+        </button>
+    </div>
+);
+
+export default hot(module)(Popup);
