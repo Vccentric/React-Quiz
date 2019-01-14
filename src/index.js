867f638c9150f069e4f940c89094d8e81c9ffb57
Vccentric <35283237+Vccentric@users.noreply.github.com>
2019-01-13 18:03:59 -0800

    Removed the 'React-Only' build and use the 'React-Redux' build by default.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import QuizContainer from "./react/QuizContainer";
-// import * as data from "../data/data.json";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import { devToolsEnhancer } from "redux-devtools-extension";
 import QuizContainer from "./redux/containers/QuizContainer";
 import rootReducer from "./redux/reducers";
-
-// using react only build
-// ReactDOM.render(<QuizContainer data={data.default} />, document.getElementById("root"));
 
 // using react-redux build
 const store = createStore(rootReducer, devToolsEnhancer());
