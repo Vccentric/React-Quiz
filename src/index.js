bcd73cccae6ece916e11d61720dc85d39cc146c3
Vccentric <35283237+Vccentric@users.noreply.github.com>
2022-05-31 22:28:02 -0700

    1. Update packages 2. Update initial render by using latest syntax: createRoot()

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import QuizApp from "./containers/QuizApp";
 import "normalize.css";
 import "./styles/quiz.scss";
 import data from "../data/quizzes.json";
 
-ReactDOM.render(<QuizApp data={data} />, document.getElementById("root"));
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<QuizApp data={data} />);
