e1d3c6e686cb16d50f7cb6d1b7924502b12ec5cf
Vccentric <35283237+Vccentric@users.noreply.github.com>
2022-11-03 00:39:48 -0700

    1. Rename SCSS file to 'ReactQuiz.scss' 2. Add basic CSS foundation for QuizList component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import ReactQuiz from "./containers/ReactQuiz";
 import "normalize.css";
-import "./styles/quiz.scss";
+import "./styles/ReactQuiz.scss";
 import data from "../data/quizzes.json";
 
 const container = document.getElementById("root");
