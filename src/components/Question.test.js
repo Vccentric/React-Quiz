c74b714c26d5fc762fbf9094b826c0d820119ed9
Vccentric <35283237+Vccentric@users.noreply.github.com>
2022-11-10 21:08:05 -0800

    Add unit tests for Question component

---
+++ src/components/Question.test.js
@@ -0,0 +1,53 @@
+import "@testing-library/jest-dom";
+import renderer from "react-test-renderer";
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import Question from "./Question";
+
+// setup variables
+const currentQuestion = {};
+const totalQuestions = 5;
+const quizStats = {
+  currentIndex: 0,
+  selectedAnswer: null,
+  showResults: false,
+  totalCorrectAnswers: 0,
+};
+const setQuizStats = jest.fn();
+const setQuizList = jest.fn();
+
+test("renders correctly - snapshot", () => {
+  const tree = renderer
+    .create(
+      <Question
+        currentQuestion={currentQuestion}
+        totalQuestions={totalQuestions}
+        quizStats={quizStats}
+        setQuizStats={setQuizStats}
+        setQuizList={setQuizList}
+      />
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test("renders results with mock values", () => {
+  const mockQuestion = {
+    question: "Question Name",
+    answerChoices: ["100", "200"],
+    correctAnswer: "100",
+  };
+  render(
+    <Question
+      currentQuestion={mockQuestion}
+      totalQuestions={totalQuestions}
+      quizStats={quizStats}
+      setQuizStats={setQuizStats}
+      setQuizList={setQuizList}
+    />
+  );
+  const item1 = screen.getByText("Question Name");
+  const item2 = screen.getByText("100");
+  expect(item1).toBeInTheDocument();
+  expect(item2).toBeInTheDocument();
+});
